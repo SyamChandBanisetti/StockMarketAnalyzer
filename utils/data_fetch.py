cb80d2b164d3e1bc556e1a4e7a1bba69b2dcb49b
Syam Chand Banisetti <syamchandbanisetti@gmail.com>
2025-08-25 19:59:25 +0530

    Update data_fetch.py

--- utils/data_fetch.py
+++ utils/data_fetch.py
@@ -1,14 +1,15 @@
 import yfinance as yf
 from datetime import datetime, timedelta
 
-end_date = datetime.today()
-start_date = end_date - timedelta(days=365)
-
 def get_stock_data(symbol):
     """Fetch last 1 year stock data from Yahoo Finance."""
     try:
+        end_date = datetime.today()
+        start_date = end_date - timedelta(days=365)
         df = yf.download(symbol, start=start_date, end=end_date)
         df.reset_index(inplace=True)
+        if df.empty or 'Close' not in df.columns:
+            return None
         return df
     except Exception as e:
         print(f"Error fetching data for {symbol}: {e}")
